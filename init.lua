53a36dda7918f4deffc040ae5ac7e20c47285bb1
root27 <oguzhaneee@gmail.com>
2024-04-30 14:22:59 +0300

    markdown plugin

--- init.lua
+++ init.lua
@@ -174,6 +174,15 @@ require('lazy').setup({
   --    require('Comment').setup({})
 
   {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
+
+  {
     'mbbill/undotree',
     lazy = false,
   },
