edd20fbcb881c41b7be2e89ae62f31861a24e364
root27 <oguzhaneee@gmail.com>
2024-05-07 10:58:04 +0300

    live server plugin remove

--- init.lua
+++ init.lua
@@ -182,15 +182,6 @@ require('lazy').setup({
     end,
   },
 
-  require('lazy').setup {
-    {
-      'barrett-ruth/live-server.nvim',
-      build = 'pnpm add -g live-server',
-      cmd = { 'LiveServerStart', 'LiveServerStop' },
-      config = true,
-    },
-  },
-
   {
     'mbbill/undotree',
     lazy = false,
