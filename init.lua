53b7ab558029039e6e83b93bd7a22f5bf3929f1f
root27 <oguzhaneee@gmail.com>
2024-06-25 17:52:41 +0300

    Copilot fix

--- init.lua
+++ init.lua
@@ -159,6 +159,7 @@ vim.opt.rtp:prepend(lazypath)
 --  To update plugins you can run
 --    :Lazy update
 --
+--
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
@@ -172,6 +173,24 @@ require('lazy').setup({
   --
   --  This is equivalent to:
   --    require('Comment').setup({})
+
+  {
+    'zbirenbaum/copilot-cmp',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot_cmp').setup()
+    end,
+    dependencies = {
+      'zbirenbaum/copilot.lua',
+      cmd = 'Copilot',
+      config = function()
+        require('copilot').setup {
+          suggestion = { enabled = false },
+          panel = { enabled = false },
+        }
+      end,
+    },
+  },
 
   {
     'barrett-ruth/live-server.nvim',
@@ -220,92 +239,6 @@ require('lazy').setup({
     config = function()
       require('rest-nvim').setup()
     end,
-  },
-
-  -- Github Copilot
-
-  {
-    recommended = true,
-    -- copilot
-    {
-      'zbirenbaum/copilot.lua',
-      cmd = 'Copilot',
-      build = ':Copilot auth',
-      opts = {
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-        filetypes = {
-          markdown = true,
-          help = true,
-        },
-      },
-    },
-    {
-      'nvim-lualine/lualine.nvim',
-      optional = true,
-      event = 'VeryLazy',
-      opts = function(_, opts)
-        local colors = {
-          [''] = LazyVim.ui.fg 'Special',
-          ['Normal'] = LazyVim.ui.fg 'Special',
-          ['Warning'] = LazyVim.ui.fg 'DiagnosticError',
-          ['InProgress'] = LazyVim.ui.fg 'DiagnosticWarn',
-        }
-        table.insert(opts.sections.lualine_x, 2, {
-          function()
-            local icon = LazyVim.config.icons.kinds.Copilot
-            local status = require('copilot.api').status.data
-            return icon .. (status.message or '')
-          end,
-          cond = function()
-            if not package.loaded['copilot'] then
-              return
-            end
-            local ok, clients = pcall(LazyVim.lsp.get_clients, { name = 'copilot', bufnr = 0 })
-            if not ok then
-              return false
-            end
-            return ok and #clients > 0
-          end,
-          color = function()
-            if not package.loaded['copilot'] then
-              return
-            end
-            local status = require('copilot.api').status.data
-            return colors[status.status] or colors['']
-          end,
-        })
-      end,
-    },
-
-    -- copilot cmp source
-    {
-      'nvim-cmp',
-      dependencies = {
-        {
-          'zbirenbaum/copilot-cmp',
-          dependencies = 'copilot.lua',
-          opts = {},
-          config = function(_, opts)
-            local copilot_cmp = require 'copilot_cmp'
-            copilot_cmp.setup(opts)
-            -- attach cmp source whenever copilot attaches
-            -- fixes lazy-loading issues with the copilot cmp source
-            LazyVim.lsp.on_attach(function(client)
-              copilot_cmp._on_insert_enter {}
-            end, 'copilot')
-          end,
-        },
-      },
-      ---@param opts cmp.ConfigSchema
-      opts = function(_, opts)
-        table.insert(opts.sources, 1, {
-          name = 'copilot',
-          group_index = 1,
-          priority = 100,
-        })
-      end,
-    },
   },
 
   -- Here is a more advanced example where we pass configuration
@@ -800,6 +733,7 @@ require('lazy').setup({
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
         },
         sources = {
+          { name = 'copilot' },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
