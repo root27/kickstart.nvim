b4879061637d0929a76b04c8639fabc20690a29d
root27 <oguzhaneee@gmail.com>
2024-07-08 09:34:40 +0300

    neo-tree plugin

--- init.lua
+++ init.lua
@@ -171,6 +171,23 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to force a plugin to be loaded.
   --
+  --
+  --
+  --
+  -- Tree File system
+  --
+  --
+
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
   --  This is equivalent to:
   --    require('Comment').setup({})
 
