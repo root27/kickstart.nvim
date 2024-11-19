65807963cbfb34861b4f8b57e65acce74730101b
root27 <oguzhaneee@gmail.com>
2024-11-19 12:16:59 +0300

    add templ file parse

--- init.lua
+++ init.lua
@@ -15,6 +15,14 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+--[[ Templ File parse --]]
+
+vim.filetype.add {
+  extension = {
+    templ = 'templ',
+  },
+}
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
